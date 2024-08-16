1e7167b404eb974842f8f59eda68b54b01f0e2be
tvicky7x <tvicky7x@gmail.com>
2024-08-16 23:20:33 +0530

    expert section done

--- src/components/ExpertSection/ExpertSection.js
+++ src/components/ExpertSection/ExpertSection.js
@@ -10,9 +10,9 @@ import Link from "next/link";
 function ExpertSection() {
   const content = getComponentText("home.expertSection");
   return (
-    <MainLayout innerClass={"py-20"}>
+    <MainLayout innerClass={"xl:py-20 md:py-14 py-12"}>
       <div className="flex items-center justify-between ">
-        <h2 className="font-semibold text-6xl flex flex-col leading-snug ">
+        <h2 className="font-semibold xl:text-6xl lg:text-5xl md:text-4xl text-3xl flex flex-col leading-snug ">
           {content.heading.map((item, index) => {
             return <span key={index}>{item}</span>;
           })}
@@ -20,27 +20,31 @@ function ExpertSection() {
         <img
           src="/svg/sittingMan.svg"
           alt="sitting man"
-          className="aspect-auto w-48"
+          className="aspect-auto xl:w-48 lg:w-36 md:w-28 w-20 "
         />
       </div>
-      <div className="grid grid-cols-3 gap-20 py-20 ">
+      <div className="grid md:grid-cols-3 grid-cols-2 xl:gap-20 lg:gap-8 md:gap-4 gap-x-0.5 gap-y-6 xl:py-20 md:py-12 py-10 ">
         {content.expertList.map((item, index) => {
           return (
             <Link
               prefetch={false}
               href={filePrefix(item.link, "/services")}
               key={index}
-              className="text-center hover:shadow-xl transition-all duration-300 ease-in-out px-2 py-4 rounded-lg "
+              className="text-center hover:shadow-xl transition-all duration-300 ease-in-out lg:px-2 px-0 py-4 rounded-lg "
             >
-              <div className="bg-black p-5 rounded-full aspect-square w-[86px] inline-flex items-center ">
+              <div className="bg-black xl:p-5 lg:p-4 p-3.5 rounded-full aspect-square xl:w-[86px] lg:w-[75px] w-[65px] inline-flex items-center ">
                 <img
                   src={svgFilePrefix(item.imageUrl)}
                   alt={item.head}
-                  className="aspect-auto w-full"
+                  className="aspect-auto w-full "
                 />
               </div>
-              <p className="font-semibold text-2xl mt-5 ">{item.head}</p>
-              <p className="text-paraSecondary mt-3 px-3 ">{item.para}</p>
+              <p className="font-semibold xl:text-2xl lg:text-xl md:text-lg text-sm lg:mt-5 mt-4 ">
+                {item.head}
+              </p>
+              <p className="text-paraSecondary lg:mt-3 mt-2 px-3 xl:text-base lg:text-sm md:text-xs text-[10px] ">
+                {item.para}
+              </p>
             </Link>
           );
         })}
