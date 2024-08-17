43fa0932b69cc0d7c633191f185f24ee1caff4a8
tvicky7x <tvicky7x@gmail.com>
2024-08-17 14:36:53 +0530

    experience section done

--- src/components/ExperienceSection/ExperienceSection.js
+++ src/components/ExperienceSection/ExperienceSection.js
@@ -23,20 +23,22 @@ function ExperienceSection() {
     <div
       style={{ height: scrollHeight * 3 }}
       ref={scrollContainer}
-      className="overflow-hidden"
+      className="md:px-[40px] px-[10px]"
     >
-      <div className="sticky top-5 py-14 max-w-[80rem] mx-auto ">
+      <div className="sticky top-5 xl:py-14 lg:py-12 md:py-10 pt-8 pb-0 max-w-[80rem] mx-auto overflow-hidden  ">
         <div className="flex items-center justify-between">
-          <h2 className="font-semibold text-6xl">{content.heading}</h2>
+          <h2 className="font-semibold xl:text-6xl lg:text-5xl md:text-4xl text-3xl ">
+            {content.heading}
+          </h2>
           <img
             src={svgFilePrefix("/questionMan.svg")}
             alt="my experience illustration"
-            className="aspect-auto w-72"
+            className="aspect-auto xl:w-72 lg:w-64 md:w-56 w-36 "
           />
         </div>
-        <div className="relative mt-14">
-          <div className="w-0 h-0 border-[15px] rounded-full border-black absolute left-0 -translate-x-full top-1/2 -translate-y-1/2 "></div>
-          <ArrowForwardRoundedIcon className="text-black text-8xl absolute right-0 translate-x-[50%] top-1/2 -translate-y-1/2" />
+        <div className="relative xl:mt-14 lg:mt-10 md:mt-8 mt-4 ">
+          <div className="w-0 h-0 xl:border-[15px] md:border-[12px] border-[8px] rounded-full border-black absolute left-0 top-1/2 -translate-y-1/2 "></div>
+          <ArrowForwardRoundedIcon className="text-black xl:text-8xl md:text-7xl text-5xl absolute right-0 translate-x-[20%] top-1/2 -translate-y-1/2" />
           <div className=" overflow-hidden">
             <motion.div
               style={{ x: translateX }}
@@ -52,9 +54,9 @@ function ExperienceSection() {
                     <div
                       className={`${
                         index % 2 === 0
-                          ? "order-none border-b-8 -mb-1 "
-                          : "order-2 border-t-8 -mt-1 "
-                      } border-black py-12 `}
+                          ? "order-none xl:border-b-8 md:border-b-[6px] border-b-[4px] xl:-mb-1 md:-mb-[3px] -mb-[1.8px] "
+                          : "order-2 xl:border-t-8 md:border-t-[6px] border-t-[4px] xl:-mt-1 md:-mt-[3px] -mt-[1.8px] "
+                      } border-black xl:py-12 md:py-10 py-8 `}
                     >
                       <motion.div
                         variants={{
@@ -80,19 +82,21 @@ function ExperienceSection() {
                         whileInView="animate"
                         // viewport={{ once: true }}
                         custom={index}
-                        className={`border-2 border-black px-4 pt-4 pb-5 relative w-[350px] mx-auto `}
+                        className={` border-2 border-black xl:px-4 px-3 lg:py-4 py-3 relative lg:w-[350px] md:w-[300px] w-full mx-auto `}
                       >
-                        <p className="font-semibold text-2xl">{item.head}</p>
-                        <p className="text-paraSecondary text-lg mt-1">
+                        <p className="font-semibold xl:text-2xl lg:text-xl md:text-lg text-base ">
+                          {item.head}
+                        </p>
+                        <p className="text-paraSecondary xl:text-lg lg:text-base md:text-sm text-xs mt-1">
                           {item.para}
                         </p>
                         {index % 2 == 0 ? (
-                          <div className="absolute w-0 h-0 border-black border-[24px] border-r-transparent border-b-transparent bottom-0 translate-y-full">
-                            <div className="absolute w-0 h-0 border-white border-[21px] border-r-transparent border-b-transparent top-0 -translate-y-[58%] left-1/2 -translate-x-[50%] "></div>
+                          <div className="absolute w-0 h-0 border-black xl:border-[24px] md:border-[20px] border-[16px] border-r-transparent border-b-transparent bottom-0 translate-y-full">
+                            <div className="absolute w-0 h-0 border-white xl:border-[21px] md:border-[18px] border-[14px] border-r-transparent border-b-transparent top-0 -translate-y-[58%] left-1/2 -translate-x-[50%] "></div>
                           </div>
                         ) : (
-                          <div className="absolute w-0 h-0 border-black border-[24px] border-t-transparent border-r-transparent top-0 -translate-y-full">
-                            <div className="absolute w-0 h-0 border-white border-[21px] border-t-transparent border-r-transparent bottom-0 translate-y-[58%] left-1/2 -translate-x-[50%] "></div>
+                          <div className="absolute w-0 h-0 border-black xl:border-[24px] md:border-[20px] border-[16px] border-t-transparent border-r-transparent top-0 -translate-y-full">
+                            <div className="absolute w-0 h-0 border-white xl:border-[21px] md:border-[18px] border-[14px] border-t-transparent border-r-transparent bottom-0 translate-y-[58%] left-1/2 -translate-x-[50%] "></div>
                           </div>
                         )}
                       </motion.div>
