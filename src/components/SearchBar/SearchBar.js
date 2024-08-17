c9b47dbcf4c3df4103f27e04b2c2f45cfe30d4e2
tvicky7x <tvicky7x@gmail.com>
2024-08-17 15:21:26 +0530

    search bar done

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -8,13 +8,13 @@ function SearchBar({ tools, resources }) {
     `util.searchBar.${tools ? "tools" : resources && "resources"}`
   );
   return (
-    <MainLayout innerClass={"pt-10"}>
+    <MainLayout innerClass={"xl:pt-10 lg:pt-8 md:pt-6 pt-5 "}>
       <div className="relative">
         <input
-          className="w-full text-xl border-paraSecondary/40 border-2 py-2 pl-14 pr-3 rounded-lg focus:outline-none peer "
+          className="w-full xl:text-xl lg:text-lg md:text-base text-sm border-paraSecondary/40 border-2 py-2 xl:pl-14 md:pl-12 pl-10 pr-3 rounded-lg focus:outline-none peer "
           placeholder={content.placeholder}
         />
-        <SearchRoundedIcon className="text-3xl text-paraSecondary peer-focus:text-black absolute top-1/2 -translate-y-1/2 left-4 " />
+        <SearchRoundedIcon className="xl:text-3xl text-2xl text-paraSecondary peer-focus:text-black absolute top-1/2 -translate-y-1/2 md:left-4 left-3 " />
       </div>
     </MainLayout>
   );
