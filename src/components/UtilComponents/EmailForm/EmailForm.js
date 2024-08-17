64ca1865e4c029301e403a235b73797923f50d49
tvicky7x <tvicky7x@gmail.com>
2024-08-17 20:55:56 +0530

    popup dialog box done

--- src/components/UtilComponents/EmailForm/EmailForm.js
+++ src/components/UtilComponents/EmailForm/EmailForm.js
@@ -8,10 +8,46 @@ function EmailForm({ home, about }) {
   const content = getComponentText("util.emailForm");
   const [isDialogOpen, setDialogOpen] = useState(false);
   const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [inValid, setInValid] = useState(false);
 
   //onchange email
-  function onChangeEmail(value) {
-    setEmail(value);
+  function onChangeValue(value, key) {
+    if (key === "name") {
+      setName(value);
+    } else if (key === "phone") {
+      setPhone(value);
+    } else if (key === "email") {
+      setEmail(value);
+      setInValid(false);
+    }
+  }
+
+  //onSubmit function api
+  function onSubmitApi(e) {
+    e.preventDefault();
+    console.log(email, phone, name);
+  }
+
+  // Check email validation function
+  function checkEmailValidation(email) {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (emailRegex.test(email)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  //onSubmit dialog box
+  function onSubmitDialogBox(e) {
+    e.preventDefault();
+    if (email !== "" && checkEmailValidation(email)) {
+      openDialog();
+    } else if (email !== "" && !checkEmailValidation(email)) {
+      setInValid(true);
+    }
   }
 
   //open dialog box
@@ -22,28 +58,51 @@ function EmailForm({ home, about }) {
   //closing dialog box
   function closeDialog() {
     setDialogOpen(false);
+    setEmail("");
+    setName("");
+    setPhone("");
+    setInValid(false);
   }
   return (
     <>
-      <PopupForm open={isDialogOpen} onClose={closeDialog} />
-      <div className="inline-flex flex-wrap justify-center lg:gap-x-3 gap-x-2 gap-y-3 lg:text-lg text-base ">
+      <PopupForm
+        open={isDialogOpen}
+        onClose={closeDialog}
+        onChangeValue={onChangeValue}
+        name={name}
+        phone={phone}
+        onSubmitApi={onSubmitApi}
+      />
+      <div
+        className={`inline-flex flex-wrap justify-center lg:gap-x-3 gap-x-2 ${
+          inValid ? "gap-y-6" : "gap-y-3"
+        }  lg:text-lg text-base `}
+      >
         {content.inputList.map((item, index) => {
           return (
-            <Input
-              key={index}
-              name={item.name}
-              placeholder={item.placeholder}
-              type={item.type}
-              trueType={item.trueType}
-              value={email}
-              onChange={onChangeEmail}
-              inputCss="text-slate-600 lg:px-3 px-2 lg:py-2 py-1.5 rounded-lg lg:w-[280px] w-[240px]"
-            />
+            <div className="relative">
+              <Input
+                key={index}
+                name={item.name}
+                placeholder={item.placeholder}
+                type={item.type}
+                trueType={item.trueType}
+                value={email}
+                onChange={onChangeValue}
+                required={true}
+                inputCss="text-slate-600 lg:px-3 px-2 lg:py-2 py-1.5 rounded-lg lg:w-[280px] w-[240px]"
+              />
+              {inValid && (
+                <p className="absolute md:text-sm text-xs md:mt-2 mt-1 w-full text-center text-red-500 ">
+                  Invalid Email
+                </p>
+              )}
+            </div>
           );
         })}
         <button
-          type="button"
-          onClick={openDialog}
+          type="submit"
+          onClick={onSubmitDialogBox}
           className="bg-primaryColor text-black lg:py-2 py-1.5 lg:px-4 px-3 rounded-lg font-semibold "
         >
           {content.button[`${home ? "home" : about && "about"}`].para}
--- src/components/UtilComponents/PopupForm/PopupForm.js
+++ src/components/UtilComponents/PopupForm/PopupForm.js
@@ -3,31 +3,24 @@ import { getComponentText } from "@/utils/functions/functions";
 import { Dialog } from "@mui/material";
 import React, { useState } from "react";
 import Input from "../Input/Input";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 
-function PopupForm({ open, onClose }) {
+function PopupForm({ open, onClose, onChangeValue, phone, name, onSubmitApi }) {
   const content = getComponentText("util.popupForm");
-  const [popupInput, setPopupInput] = useState({ name: "", phone: "" });
-
-  //onchange input
-  function onChangePopupInput(value, key) {
-    console.log(key, value);
-    if (key === "name") {
-      setPopupInput((previousInput) => {
-        return { ...previousInput, [key]: value };
-      });
-    } else if (key === "phone") {
-      setPopupInput((previousInput) => {
-        return { ...previousInput, [key]: value };
-      });
-    }
-  }
 
   return (
-    <Dialog
-      open={open}
-      onClose={onClose}>
-      <div>
-        <form className="flex flex-col">
+    <Dialog open={open} onClose={onClose}>
+      <div className="w-[420px] relative p-6 ">
+        <CloseRoundedIcon
+          onClick={onClose}
+          className="absolute top-1 right-1 text-3xl cursor-pointer "
+        />
+        <h2 className="flex flex-col font-bold text-2xl items-center ">
+          {content.heading.map((item, index) => {
+            return <span key={index}>{item}</span>;
+          })}
+        </h2>
+        <form className="flex flex-col gap-y-3 mt-8  ">
           {content.inputList.map((item, index) => {
             return (
               <Input
@@ -36,17 +29,23 @@ function PopupForm({ open, onClose }) {
                 placeholder={item.placeholder}
                 type={item.type}
                 trueType={item.trueType}
-                value={popupInput[`${item.name}`]}
-                onChange={onChangePopupInput}
-                inputCss="text-slate-600"
+                value={
+                  item.name === "name" ? name : item.name === "phone" && phone
+                }
+                onChange={onChangeValue}
+                inputCss="text-slate-600 lg:px-3 px-2 lg:py-2 py-1.5 rounded-lg border-2 border-slate-200/70 "
               />
             );
           })}
-          <button
-            type="button"
-            className="bg-primaryColor text-black ">
-            {content.button.para}
-          </button>
+          <div className="flex items-center justify-center mt-3">
+            <button
+              type="submit"
+              onClick={onSubmitApi}
+              className="bg-primaryColor text-black px-20 py-3 rounded font-medium "
+            >
+              {content.button.para}
+            </button>
+          </div>
         </form>
       </div>
     </Dialog>
