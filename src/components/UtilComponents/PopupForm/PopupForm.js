ef971a5631d54dbccc696457061b4d86c3770429
tvicky7x <tvicky7x@gmail.com>
2024-08-17 21:06:00 +0530

    popup dialog box responsive

--- src/components/UtilComponents/PopupForm/PopupForm.js
+++ src/components/UtilComponents/PopupForm/PopupForm.js
@@ -9,18 +9,18 @@ function PopupForm({ open, onClose, onChangeValue, phone, name, onSubmitApi }) {
   const content = getComponentText("util.popupForm");
 
   return (
-    <Dialog open={open} onClose={onClose}>
-      <div className="w-[420px] relative p-6 ">
+    <Dialog open={open}>
+      <div className="lg:w-[420px] md:w-[380px] w-full relative md:p-6 p-4 ">
         <CloseRoundedIcon
           onClick={onClose}
-          className="absolute top-1 right-1 text-3xl cursor-pointer "
+          className="absolute top-1 right-1 md:text-3xl text-2xl cursor-pointer "
         />
-        <h2 className="flex flex-col font-bold text-2xl items-center ">
+        <h2 className="flex flex-col font-bold md:text-2xl text-xl items-center ">
           {content.heading.map((item, index) => {
             return <span key={index}>{item}</span>;
           })}
         </h2>
-        <form className="flex flex-col gap-y-3 mt-8  ">
+        <form className="flex flex-col gap-y-3 md:mt-8 mt-6 ">
           {content.inputList.map((item, index) => {
             return (
               <Input
@@ -37,11 +37,11 @@ function PopupForm({ open, onClose, onChangeValue, phone, name, onSubmitApi }) {
               />
             );
           })}
-          <div className="flex items-center justify-center mt-3">
+          <div className="flex justify-center md:mt-3 mt-2">
             <button
               type="submit"
               onClick={onSubmitApi}
-              className="bg-primaryColor text-black px-20 py-3 rounded font-medium "
+              className="bg-primaryColor text-black lg:px-20 px-16 md:w-auto w-full lg:py-3 py-2 rounded font-medium "
             >
               {content.button.para}
             </button>
