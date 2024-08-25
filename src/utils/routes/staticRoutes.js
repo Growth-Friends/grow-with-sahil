6aab183cdbbd0d779e2df337acd9e645b6de92bd
tvicky7x <tvicky7x@gmail.com>
2024-08-25 20:43:43 +0530

    navbar done

--- src/utils/routes/staticRoutes.js
+++ src/utils/routes/staticRoutes.js
@@ -2,6 +2,7 @@ const staticRoutes = {
   home: "/",
   about: "/about",
   services: "/services",
+  contact: "/contact",
   marketingTools: "/marketing-tools",
   growthResources: "/growth-resources",
   blog: "https://growthfrens.com/blog/",
