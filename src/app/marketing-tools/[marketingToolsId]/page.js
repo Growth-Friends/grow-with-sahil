d21cc86cbfc17f77ab3583e4a00b3546b049dc77
tvicky7x <tvicky7x@gmail.com>
2024-08-27 15:48:33 +0530

    static site generation done

--- src/app/marketing-tools/[marketingToolsId]/page.js
+++ src/app/marketing-tools/[marketingToolsId]/page.js
@@ -5,12 +5,12 @@ import MoreResources from "@/components/ResourcesComponent/MoreResources/MoreRes
 import SubHeroSection from "@/components/SubHeroSection/SubHeroSection";
 import React from "react";
 
-function IndividualMarketingToolsPage() {
+function IndividualMarketingToolsPage({ params }) {
   return (
     <>
-      <SubHeroSection />
+      {/* <SubHeroSection /> */}
       <DummyToolsSection />
-      <DetailSection />
+      {/* <DetailSection /> */}
       <MoreResources moreTools={true} />
       <AboutMeSection />
     </>
@@ -18,3 +18,15 @@ function IndividualMarketingToolsPage() {
 }
 
 export default IndividualMarketingToolsPage;
+
+export const dynamicParams = false;
+
+export async function generateStaticParams() {
+  const staticList = await fetch(
+    "https://growwithsahil.com/blog/wp-json/wp/v2/tools-api/?_fields=acf,content,slug&acf_format=standard"
+  ).then((res) => res.json());
+
+  return staticList.map((item) => ({
+    marketingToolsId: item.slug,
+  }));
+}
