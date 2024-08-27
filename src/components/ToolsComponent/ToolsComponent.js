58c1c17e6052fc72d8c85541606d384a8b9a5cdb
tvicky7x <tvicky7x@gmail.com>
2024-08-27 10:27:20 +0530

    api for tools page done

--- src/components/ToolsComponent/ToolsComponent.js
+++ src/components/ToolsComponent/ToolsComponent.js
@@ -9,7 +9,7 @@ import Link from "next/link";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 const ToolsComponent = forwardRef(
-  ({ seoTools, socialTools, performanceTools }, ref) => {
+  ({ seoTools, socialTools, performanceTools, toolsList }, ref) => {
     const content = getComponentText(
       `util.${
         seoTools
@@ -19,6 +19,7 @@ const ToolsComponent = forwardRef(
           : performanceTools && "performanceTools"
       }`
     );
+    console.log(toolsList);
     return (
       <MainLayout>
         <div className="xl:py-10 py-7 overflow-hidden ">
@@ -49,29 +50,28 @@ const ToolsComponent = forwardRef(
             </p>
           </div>
           <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 xl:p-10 lg:p-8 md:p-6 p-6 xl:gap-10 lg:gap-8 md:gap-6 gap-6 border-x-2 border-b-2 border-paraSecondary/40 rounded-b-2xl ">
-            {content.toolsList.map((item, index) => {
+            {toolsList.map((item, index) => {
               return (
                 <Link
                   prefetch={false}
-                  href={filePrefix(item.link, "/marketing-tools")}
+                  href={filePrefix(`/${item.slug}`, "/marketing-tools")}
                   key={index}
                   className="bg-black text-white xl:p-8 p-4 gap-x-5 rounded-xl flex flex-col xl:max-w-[320px] lg:max-w-[280px] md:max-w-[260px] max-w-[250px] mx-auto "
                 >
                   <div className="flex items-center lg:gap-x-6 gap-x-5">
                     <img
                       loading="lazy"
-                      src={svgFilePrefix(item.imageUrl)}
-                      alt={item.head}
+                      // src={svgFilePrefix(item.imageUrl)}
+                      src={item.acf.tools_icon}
+                      alt={item.acf.tools_name}
                       className="aspect-auto xl:w-16 lg:w-14 w-12 "
                     />
                     <p className="font-semibold xl:text-2xl md:text-xl text-lg ">
-                      {item.head}
+                      {item.acf.tools_name}
                     </p>
                   </div>
                   <div className="flex items-center justify-between xl:text-base text-sm mt-5">
-                    <p className="font-light">
-                      {item.hostedInHouse ? "In-House" : "Re-directed"}
-                    </p>
+                    <p className="font-light">{item.acf.tools_type}</p>
                     <div className="flex items-center gap-x-1">
                       <img
                         loading="lazy"
@@ -79,15 +79,15 @@ const ToolsComponent = forwardRef(
                         src={svgFilePrefix("/likeSymbol.svg")}
                         className="aspect-auto w-4"
                       />
-                      <span>{item.likes}</span>
+                      <span>{item.acf.downloads}</span>
                     </div>
                   </div>
-                  <p className="xl:text-lg md:text-base text-sm mt-5 mb-10 ">
-                    {item.aboutTool}
+                  <p className="xl:text-lg md:text-base text-sm mt-5 mb-10 line-clamp-3 ">
+                    {item.acf.out_description}
                   </p>
                   <div className="flex justify-center mt-auto  ">
                     <button className="bg-primaryColor text-black px-3 py-2 rounded-md font-medium flex xl:text-base md:text-sm text-xs items-center gap-x-1 hover:gap-x-2 transition-all duration-300 ease-in-out ">
-                      {item.button}
+                      {item.acf.cta_text}
                       <ArrowForwardIcon className="text-base" />
                     </button>
                   </div>
--- src/components/ToolsComponent/ToolsWrapper/ToolsWrapper.js
+++ src/components/ToolsComponent/ToolsWrapper/ToolsWrapper.js
@@ -1,6 +1,7 @@
 "use client";
-import React, { useRef } from "react";
+import React, { use, useEffect, useRef, useState } from "react";
 import ToolsComponent from "../ToolsComponent";
+import getMethodCall from "@/utils/services/services";
 
 export let seoToolsRef, socialToolsRef, performanceToolsRef;
 
@@ -8,11 +9,47 @@ function ToolsWrapper() {
   seoToolsRef = useRef();
   socialToolsRef = useRef();
   performanceToolsRef = useRef();
+  const [seoToolsList, setSeoToolsList] = useState();
+  const [performanceToolsList, setPerformanceToolsList] = useState();
+  const [socialToolsList, setSocialToolsList] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const response = await getMethodCall(
+        "https://growwithsahil.com/blog/wp-json/wp/v2/tools-api/?_fields=acf,content,slug&acf_format=standard"
+      );
+      const data = await response.json();
+      setSeoToolsList(
+        data.filter((item) => {
+          return item.acf.tool_category === "Search Engine optimization";
+        })
+      );
+    })();
+  }, []);
+
   return (
     <>
-      <ToolsComponent seoTools={true} ref={seoToolsRef} />
-      <ToolsComponent socialTools={true} ref={socialToolsRef} />
-      <ToolsComponent performanceTools={true} ref={performanceToolsRef} />
+      {seoToolsList && (
+        <ToolsComponent
+          seoTools={true}
+          ref={seoToolsRef}
+          toolsList={seoToolsList}
+        />
+      )}
+      {socialToolsList && (
+        <ToolsComponent
+          socialTools={true}
+          ref={socialToolsRef}
+          toolsList={socialToolsList}
+        />
+      )}
+      {performanceToolsList && (
+        <ToolsComponent
+          performanceTools={true}
+          ref={performanceToolsRef}
+          toolsList={performanceToolsList}
+        />
+      )}
     </>
   );
 }
